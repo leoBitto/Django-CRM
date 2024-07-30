0aecc43d518ba5721e730ca60c59b483f72193e7
leoBitto <leonardobitto@gmail.com>
2024-07-30 16:57:01 +0200

    added admin models

--- admin.py
+++ admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Company, Supplier, Customer
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    search_fields = ('name', 'address')
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'email', 'phone')
+    search_fields = ('name', 'email', 'phone')
+    list_filter = ('company',)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'email', 'phone', 'status')
+    search_fields = ('name', 'email', 'phone')
+    list_filter = ('company', 'status')
