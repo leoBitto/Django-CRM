8db0acdd149f7e4b4d92c1c2546dc5b15cae7a0c
leoBitto <leonardobitto@gmail.com>
2025-03-01 18:33:22 +0100

    added basic views

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views.base import CompanyView, SupplierView, CustomerView, CompanyCategoryView
-from crm.views.aggregated import CRMReportView
+from crm.views import CompanyListView, CompanyDetailView, PersonListView, PersonDetailView, CompanyCategoryListView, CompanyCategoryDetailView
 
 app_name = 'crm'
 
 urlpatterns = [
-    path('company_categories/', CompanyCategoryView.as_view(), name='company_categories_view'),
-    path('companies/', CompanyView.as_view(), name='company_view'),
-    path('person/', PersonView.as_view(), name='person_view'),
-
+    path('companies/', CompanyListView.as_view(), name='company_list'),
+    path('companies/<int:company_id>/', CompanyDetailView.as_view(), name='company_detail'),
+    path('person/', PersonListView.as_view(), name='person_list'),
+    path('person/<int:person_id>/', PersonDetailView.as_view(), name='person_detail'),
+    path('company_categories/', CompanyCategoryListView.as_view(), name='category_list'),
+    path('company_categories/<int:category_id>/', CompanyCategoryDetailView.as_view(), name='category_detail'),
 ]
--- views/base.py
+++ views/base.py
@@ -1,278 +1,139 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
-from django.urls import reverse_lazy
-from ..models.base import Company, Supplier, Customer, CompanyCategory
-from ..forms import CompanyForm, SupplierForm, CustomerForm, CompanyCategoryForm
-from django.contrib import messages
+from crm.models import Company, Person, CompanyCategory
+from crm.forms import CompanyForm, PersonForm, CompanyCategoryForm
 
+class CompanyListView(View):
+    template_name = 'crm/companies.html'
 
-class CompanyCategoryView(LoginRequiredMixin, View):
-    template_name = 'crm/company_category.html'
-
-    def get(self, request, *args, **kwargs):
-        categories = CompanyCategory.objects.all()
-        category_info = []
-
-        # Creare un modulo per ogni categoria
-        for category in categories:
-            form = CompanyCategoryForm(instance=category)
-            category_info.append({'category': category, 'form': form})
-
-        # Aggiungere un modulo vuoto per la creazione di nuove categorie
-        create_form = CompanyCategoryForm()
-
-        return render(request, self.template_name, {
-            'category_info': category_info,
-            'create_form': create_form,
-        })
-
-    def post(self, request, *args, **kwargs):
-        if 'create_category' in request.POST:
-            category_form = CompanyCategoryForm(request.POST)
-            if category_form.is_valid():
-                category_form.save()
-                messages.success(request, 'Categoria aziendale creata con successo.')
-                return redirect('crm:company_categories_view')
-            else:
-                for field, errors in category_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'update_category' in request.POST:
-            category_id = request.POST.get('category_id')
-            category = get_object_or_404(CompanyCategory, id=category_id)
-            category_form = CompanyCategoryForm(request.POST, instance=category)
-            if category_form.is_valid():
-                category_form.save()
-                messages.success(request, 'Categoria aziendale aggiornata con successo.')
-                return redirect('crm:company_categories_view')
-            else:
-                for field, errors in category_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'delete_category' in request.POST:
-            category_id = request.POST.get('category_id')
-            category = get_object_or_404(CompanyCategory, id=category_id)
-            category.delete()
-            messages.success(request, 'Categoria aziendale eliminata con successo.')
-            return redirect('crm:company_categories_view')
-        
-        # Se si arriva qui, non è stato fatto nulla di valido
-        categories = CompanyCategory.objects.all()
-        category_info = []
-
-        for category in categories:
-            form = CompanyCategoryForm(instance=category)
-            category_info.append({'category': category, 'form': form})
-
-        create_form = CompanyCategoryForm()
-
-        return render(request, self.template_name, {
-            'category_info': category_info,
-            'create_form': create_form,
-        })
-
-
-class CompanyView(LoginRequiredMixin, View):
-    template_name = 'crm/company.html'
-    
     def get(self, request, *args, **kwargs):
         companies = Company.objects.all()
-        company_info = []
-
-        # Creare un modulo per ogni azienda
-        for company in companies:
-            form = CompanyForm(instance=company)
-            company_info.append({'company': company, 'form': form})
-
-        # Aggiungere un modulo vuoto per la creazione di nuove aziende
-        create_form = CompanyForm()
-
-        return render(request, self.template_name, {
-            'company_info': company_info,
-            'create_form': create_form,
-        })
+        form = CompanyForm()
+        return render(request, self.template_name, {'companies': companies, 'form': form})
 
     def post(self, request, *args, **kwargs):
-        if 'create_company' in request.POST:
-            company_form = CompanyForm(request.POST)
-            if company_form.is_valid():
-                company_form.save()
-                messages.success(request, 'Azienda creata con successo.')
-                return redirect('crm:company_view')
-            else:
-                for field, errors in company_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'update_company' in request.POST:
-            company_id = request.POST.get('company_id')
-            company = get_object_or_404(Company, id=company_id)
-            company_form = CompanyForm(request.POST, instance=company)
-            if company_form.is_valid():
-                company_form.save()
-                messages.success(request, 'Azienda aggiornata con successo.')
-                return redirect('crm:company_view')
-            else:
-                for field, errors in company_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'delete_company' in request.POST:
-            company_id = request.POST.get('company_id')
-            company = get_object_or_404(Company, id=company_id)
+        if 'delete_object' in request.POST:
+            company = get_object_or_404(Company, id=request.POST.get('delete_object'))
             company.delete()
-            messages.success(request, 'Azienda eliminata con successo.')
-            return redirect('crm:company_view')
-        
-        # Se si arriva qui, non è stato fatto nulla di valido
+            return redirect('crm:company_list')
+
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crm:company_list')
+
         companies = Company.objects.all()
-        company_info = []
+        return render(request, self.template_name, {'companies': companies, 'form': form})
 
-        for company in companies:
-            form = CompanyForm(instance=company)
-            company_info.append({'company': company, 'form': form})
+class CompanyDetailView(View):
+    template_name = 'crm/company_detail.html'
 
-        create_form = CompanyForm()
+    def get(self, request, company_id, *args, **kwargs):
+        company = get_object_or_404(Company, id=company_id)
+        form = CompanyForm(instance=company)
+        return render(request, self.template_name, {'company': company, 'form': form})
 
-        return render(request, self.template_name, {
-            'company_info': company_info,
-            'create_form': create_form,
-        })
+    def post(self, request, company_id, *args, **kwargs):
+        company = get_object_or_404(Company, id=company_id)
 
+        if 'update_company' in request.POST:
+            form = CompanyForm(request.POST, instance=company)
+            if form.is_valid():
+                form.save()
+                return redirect('crm:company_list')
 
-class SupplierView(LoginRequiredMixin, View):
-    template_name = 'crm/supplier.html'
-    
+        elif 'delete_company' in request.POST:
+            company.delete()
+            return redirect('crm:company_list')
+
+        return render(request, self.template_name, {'company': company, 'form': form})
+
+class PersonListView(View):
+    template_name = 'crm/people.html'
+
     def get(self, request, *args, **kwargs):
-        suppliers = Supplier.objects.all()
-        supplier_info = []
-
-        # Creare un modulo per ogni fornitore
-        for supplier in suppliers:
-            form = SupplierForm(instance=supplier)
-            supplier_info.append({'supplier': supplier, 'form': form})
-
-        # Aggiungere un modulo vuoto per la creazione di nuovi fornitori
-        create_form = SupplierForm()
-
-        return render(request, self.template_name, {
-            'supplier_info': supplier_info,
-            'create_form': create_form,
-        })
+        people = Person.objects.all()
+        form = PersonForm()
+        return render(request, self.template_name, {'people': people, 'form': form})
 
     def post(self, request, *args, **kwargs):
-        if 'create_supplier' in request.POST:
-            supplier_form = SupplierForm(request.POST)
-            if supplier_form.is_valid():
-                supplier_form.save()
-                messages.success(request, 'Fornitore creato con successo.')
-                return redirect('crm:supplier_view')
-            else:
-                for field, errors in supplier_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'update_supplier' in request.POST:
-            supplier_id = request.POST.get('supplier_id')
-            supplier = get_object_or_404(Supplier, id=supplier_id)
-            supplier_form = SupplierForm(request.POST, instance=supplier)
-            if supplier_form.is_valid():
-                supplier_form.save()
-                messages.success(request, 'Fornitore aggiornato con successo.')
-                return redirect('crm:supplier_view')
-            else:
-                for field, errors in supplier_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'delete_supplier' in request.POST:
-            supplier_id = request.POST.get('supplier_id')
-            supplier = get_object_or_404(Supplier, id=supplier_id)
-            supplier.delete()
-            messages.success(request, 'Fornitore eliminato con successo.')
-            return redirect('crm:supplier_view')
-        
-        # Se si arriva qui, non è stato fatto nulla di valido
-        suppliers = Supplier.objects.all()
-        supplier_info = []
+        if 'delete_object' in request.POST:
+            person = get_object_or_404(Person, id=request.POST.get('delete_object'))
+            person.delete()
+            return redirect('crm:person_list')
 
-        for supplier in suppliers:
-            form = SupplierForm(instance=supplier)
-            supplier_info.append({'supplier': supplier, 'form': form})
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crm:person_list')
 
-        create_form = SupplierForm()
+        people = Person.objects.all()
+        return render(request, self.template_name, {'people': people, 'form': form})
 
-        return render(request, self.template_name, {
-            'supplier_info': supplier_info,
-            'create_form': create_form,
-        })
+class PersonDetailView(View):
+    template_name = 'crm/person_detail.html'
 
-class CustomerView(LoginRequiredMixin, View):
-    template_name = 'crm/customer.html'
-    
+    def get(self, request, person_id, *args, **kwargs):
+        person = get_object_or_404(Person, id=person_id)
+        form = PersonForm(instance=person)
+        return render(request, self.template_name, {'person': person, 'form': form})
+
+    def post(self, request, person_id, *args, **kwargs):
+        person = get_object_or_404(Person, id=person_id)
+
+        if 'update_person' in request.POST:
+            form = PersonForm(request.POST, instance=person)
+            if form.is_valid():
+                form.save()
+                return redirect('crm:person_list')
+
+        elif 'delete_person' in request.POST:
+            person.delete()
+            return redirect('crm:person_list')
+
+        return render(request, self.template_name, {'person': person, 'form': form})
+
+class CompanyCategoryListView(View):
+    template_name = 'crm/categories.html'
+
     def get(self, request, *args, **kwargs):
-        customers = Customer.objects.all()
-        customer_info = []
-
-        # Creare un modulo per ogni cliente
-        for customer in customers:
-            form = CustomerForm(instance=customer)
-            customer_info.append({'customer': customer, 'form': form})
-
-        # Aggiungere un modulo vuoto per la creazione di nuovi clienti
-        create_form = CustomerForm()
-
-        return render(request, self.template_name, {
-            'customer_info': customer_info,
-            'create_form': create_form,
-        })
+        categories = CompanyCategory.objects.all()
+        form = CompanyCategoryForm()
+        return render(request, self.template_name, {'categories': categories, 'form': form})
 
     def post(self, request, *args, **kwargs):
-        if 'create_customer' in request.POST:
-            customer_form = CustomerForm(request.POST)
-            if customer_form.is_valid():
-                customer_form.save()
-                messages.success(request, 'Cliente creato con successo.')
-                return redirect('crm:customer_view')
-            else:
-                for field, errors in customer_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'update_customer' in request.POST:
-            customer_id = request.POST.get('customer_id')
-            customer = get_object_or_404(Customer, id=customer_id)
-            customer_form = CustomerForm(request.POST, instance=customer)
-            if customer_form.is_valid():
-                customer_form.save()
-                messages.success(request, 'Cliente aggiornato con successo.')
-                return redirect('crm:customer_view')
-            else:
-                for field, errors in customer_form.errors.items():
-                    for error in errors:
-                        messages.error(request, f"Errore nel campo '{field}': {error}")
-        
-        elif 'delete_customer' in request.POST:
-            customer_id = request.POST.get('customer_id')
-            customer = get_object_or_404(Customer, id=customer_id)
-            customer.delete()
-            messages.success(request, 'Cliente eliminato con successo.')
-            return redirect('crm:customer_view')
-        
-        # Se si arriva qui, non è stato fatto nulla di valido
-        customers = Customer.objects.all()
-        customer_info = []
+        if 'delete_object' in request.POST:
+            category = get_object_or_404(CompanyCategory, id=request.POST.get('delete_object'))
+            category.delete()
+            return redirect('crm:category_list')
 
-        for customer in customers:
-            form = CustomerForm(instance=customer)
-            customer_info.append({'customer': customer, 'form': form})
+        form = CompanyCategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crm:category_list')
 
-        create_form = CustomerForm()
+        categories = CompanyCategory.objects.all()
+        return render(request, self.template_name, {'categories': categories, 'form': form})
 
-        return render(request, self.template_name, {
-            'customer_info': customer_info,
-            'create_form': create_form,
-        })
+class CompanyCategoryDetailView(View):
+    template_name = 'crm/category_detail.html'
+
+    def get(self, request, category_id, *args, **kwargs):
+        category = get_object_or_404(CompanyCategory, id=category_id)
+        form = CompanyCategoryForm(instance=category)
+        return render(request, self.template_name, {'category': category, 'form': form})
+
+    def post(self, request, category_id, *args, **kwargs):
+        category = get_object_or_404(CompanyCategory, id=category_id)
+
+        if 'update_category' in request.POST:
+            form = CompanyCategoryForm(request.POST, instance=category)
+            if form.is_valid():
+                form.save()
+                return redirect('crm:category_list')
+
+        elif 'delete_category' in request.POST:
+            category.delete()
+            return redirect('crm:category_list')
+
+        return render(request, self.template_name, {'category': category, 'form': form})
